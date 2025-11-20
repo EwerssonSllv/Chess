24b5e677fe89cadb7c1067f18e84ff33105e4e3d
EwerssonSllv <ewerssonsllv@gmail.com>
2025-11-20 16:07:28 -0300

    ChessPosition added

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -7,22 +7,10 @@ namespace Chess {
     class Program {
         static void Main(string[] args) {
 
-            try {
+            ChessPosition chessPosition = new ChessPosition('a', 1);
 
-                Board board = new Board(8, 8);
-
-                board.Place(new Tower(Color.BLACK, board), new Position(0, 0));
-                board.Place(new Tower(Color.BLACK, board), new Position(2, 9));
-                board.Place(new King(Color.BLACK, board), new Position(0, 2));
-
-                Screen.printBoard(board);
-
-            } catch (BoardException e) {
-
-                Console.WriteLine(e.Message);
-
-            }
-
+            Console.WriteLine(chessPosition.toPosition());
+            Console.ReadLine();
 
         }
     }
